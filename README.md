bbe8f107eb1aeeb425c0cdcd1c8ee1c9b9b68ed1
simonmoellenbeck07 <simonmoellenbeck07@gmail.com>
2026-09-16 01:03:54 +0200

    version1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # smims26-web
 Eine wunderschöne Webseite für SMIMS im Jahr 2026
+
+https://smims26-web.onrender.com/
--- server.py
+++ server.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import time
+from math import floor
 
 app = Flask(__name__)
 
+state_text = "Griechischer Wein ist so wie das Blut der Erde"
+last_change = 0
+
+cooldown_minutes = 5
+
+def minutes_since_last_change():
+	now = time.time()
+	return floor((now - last_change) / 60)
+
 @app.route("/")
 def index():
-	return render_template("index.html")
+	minutes_elapsed = minutes_since_last_change()
+
+	return render_template(
+		"index.html",
+		state_text = state_text,
+		minutes_next_change = cooldown_minutes - minutes_elapsed,
+		can_change = minutes_elapsed >= cooldown_minutes
+	)
+
+@app.route("/changetext", methods = ["POST"])
+def change_text():
+	global state_text, last_change
+
+	if minutes_since_last_change() >= cooldown_minutes:
+		state_text = request.form["text"]
+		last_change = time.time()
+	
+	return redirect("/")
 
 if __name__ == "__main__":
 	app.run(port = 1234, debug = True)
