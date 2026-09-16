099bd395d000a06bc66eb7da2eb2e990bf768692
simonmoellenbeck07 <simonmoellenbeck07@gmail.com>
2026-09-16 09:51:31 +0200

    fix

--- server.py
+++ server.py
@@ -11,13 +11,23 @@ cooldown_minutes = 5
 
 def minutes_since_last_change():
 	now = time.time()
-	last_change = float(rd.get("state_text_last_change").decode())
+	result = rd.get("state_text_last_change")
+	if result is None:
+		rd.set("state_text_last_change", "0.0")
+		last_change = 0.0
+	else:
+		last_change = float(result.decode())
 	return floor((now - last_change) / 60)
 
 @app.route("/")
 def index():
 	minutes_elapsed = minutes_since_last_change()
-	state_text = rd.get("state_text_value").decode()
+	result = rd.get("state_text_value")
+	if result is None:
+		state_text = "Griechischer Wein ist so wie das Blut der Erde"
+		rd.set("state_text_value", state_text)
+	else:
+		state_text = result.decode()
 
 	return render_template(
 		"index.html",
