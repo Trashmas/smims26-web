9e928e3bbe90778fb8bc9633b2ba324f9eb2efed
simonmoellenbeck07 <simonmoellenbeck07@gmail.com>
2026-09-16 09:43:44 +0200

    hello

--- server.py
+++ server.py
@@ -1,21 +1,23 @@
 from flask import Flask, render_template, request, redirect
 import time
 from math import floor
+from os import environ
+import redis
 
 app = Flask(__name__)
 
-state_text = "Griechischer Wein ist so wie das Blut der Erde"
-last_change = 0
-
+rd = redis.from_url(environ["KV_URL"])
 cooldown_minutes = 5
 
 def minutes_since_last_change():
 	now = time.time()
+	last_change = float(rd.get("state_text_last_change").decode())
 	return floor((now - last_change) / 60)
 
 @app.route("/")
 def index():
 	minutes_elapsed = minutes_since_last_change()
+	state_text = rd.get("state_text_value").decode()
 
 	return render_template(
 		"index.html",
@@ -26,11 +28,9 @@ def index():
 
 @app.route("/changetext", methods = ["POST"])
 def change_text():
-	global state_text, last_change
-
 	if minutes_since_last_change() >= cooldown_minutes:
-		state_text = request.form["text"]
-		last_change = time.time()
+		rd.set("state_text_value", request.form["text"])
+		rd.set("state_text_last_change", str(time.time()))
 	
 	return redirect("/")
 
