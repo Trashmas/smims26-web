239036a854ca2811154e529f5884b7b409f4cd69
Karl Marx <keinplan@kommunismus.de>
2026-09-16 16:51:41 +0200

    fuck

--- kv_redis.py
+++ kv_redis.py
@@ -7,17 +7,17 @@ _key_last_change = "state_text_last_change"
 class Bridge:
 	def __init__(self, default_text):
 		self.rd = redis.from_url(environ["KV_URL"])
-		if rd.get(_key_state_text) is None:
-			rd.set(_key_state_text, default_text)
-		if rd.get(_key_last_change) is None:
-			rd.set(_key_last_change, "0.0")
+		if self.rd.get(_key_state_text) is None:
+			self.rd.set(_key_state_text, default_text)
+		if self.rd.get(_key_last_change) is None:
+			self.rd.set(_key_last_change, "0.0")
 	
 	def get_text(self):
-		return rd.get(_key_state_text).decode()
+		return self.rd.get(_key_state_text).decode()
 	
 	def get_last_change(self):
-		return float(rd.get(_key_last_change).decode())
+		return float(self.rd.get(_key_last_change).decode())
 	
 	def set_text_and_update_time(self, text):
-		rd.set(_key_state_text, text)
-		rd.set(_key_last_change, str(time.time()))
+		self.rd.set(_key_state_text, text)
+		self.rd.set(_key_last_change, str(time.time()))
